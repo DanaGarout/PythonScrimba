e6c90190366d73ffdfa6335df2bbc4d903f1b048
DanaGarout <danagarout5@gmail.com>
2021-11-13 17:28:39 +0300

    l

--- index.py
+++ index.py
@@ -1,2 +1,33 @@
 
+# Printstatements
+print("Welcome to Python")
+print("Welcome to python2")
+print("Create hammer")
+print("Use Hammer")
 
+# Variables
+failed_subjects="2"
+name='John'
+print('Dear Mrs Badger')
+print('Your son ' + name + ' is failing ' + failed_subjects + ' subjects.')
+print(name + '  will need to redo ' + failed_subjects + '  courses.')
+name="Eric"
+print(name + '  is doing well in geography.')
+
+# Datatypes & Tpyecasting
+print(type('hello'))
+print(type(1))
+print(type(1.64))
+print(type(True))
+
+# DataTupes exercise
+
+#Create appropriate Variables for Item name, the price 
+#and how many you have in stock
+item_name = 'widget'
+price = 23.5
+inventory = 100
+is_in_inventory = True
+print(item_name, price, inventory)
+
+# Arithmetic Operations
