fe13e97e4da63f00a0a551d2aa5d42b911bb2e4c
DanaGarout <danagarout5@gmail.com>
2021-11-14 08:17:32 +0300

    d

--- index.py
+++ index.py
@@ -1,5 +1,8 @@
 
 # Printstatements
+from typing import AsyncGenerator
+
+
 print("Welcome to Python")
 print("Welcome to python2")
 print("Create hammer")
@@ -31,3 +34,67 @@ is_in_inventory = True
 print(item_name, price, inventory)
 
 # Arithmetic Operations
+print("Basic Arithmetic")
+a=10
+b=3
+print('Addition : ', a + b)
+print('Subtraction : ', a - b)
+print('Multiplication : ', a * b)
+print('Division (float) : ', a / b)
+print('Division (floor) : ', a // b)
+print('Modulus : ', a % b)
+print('Exponent : ', a ** b)
+
+
+# Strings-Basics/Slicing
+msg='welcome to Python 101:strings'
+print(msg)
+print(msg.upper())
+print(msg.lower())
+print(msg.capitalize())
+print(msg.title())
+print(len(msg))
+print(msg.count('o'))
+
+# slicing
+print(msg[0])
+print(msg[-2])
+print(msg[2:15])
+# Exercise
+msg1=msg[18]+' '+msg[:8]+msg[25:29]+msg[7:11]+msg[13]+msg[12]+msg[2]+msg[1]+msg[-5]  
+print(msg1.title())
+print(msg1[::-1].title())
+# Strings-2 Find/replace,string formatting
+print(msg.find('Python'))
+print(msg.replace('Python','C'))
+print('Python' in msg)
+print('Python' not in msg)
+
+# User input
+print("User Input")
+name = input('What is your name?:')
+age = input('What is your age?:')
+print('Hello '+ name + '! You are '+ age + ' years old.')
+
+num1 = input('Enter a digit:')
+num2 = input('Another digit:')
+answer = float(num1) + float(num2)
+print(answer)
+
+# Exercise
+name = input('Enter your name: ')
+distance_km = input('Enter distance in km: ')
+distance_mi = float(distance_km)/1.609
+print(f'Hi {name.title()}! {distance_km}km is equivalent to {round(distance_mi,1)} miles.')
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ tempCodeRunnerFile.py
@@ -0,0 +1,15 @@
+print("User Input")
+# name = input('What is your name?:')
+# age = input('What is your age?:')
+# print('Hello '+ name + '! You are '+ age + ' years old.')
+
+# num1 = input('Enter a digit:')
+# num2 = input('Another digit:')
+# answer = float(num1) + float(num2)
+# print(answer)
+
+# # Exercise
+# name = input('Enter your name: ')
+# distance_km = input('Enter distance in km: ')
+# distance_mi = float(distance_km)/1.609
+# print(f'Hi {name.title()}! {distance_km}km is equivalent to {round(distance_mi,1)} miles.')
